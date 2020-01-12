be5c33f0fc54b7d5dabab54f66df9f33d369cb2e
Lucas Miller <lucasmiller@Lucass-MBP.attlocal.net>
2020-01-12 12:42:57 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// var hamIcon = document.getElementsByTagName("svg")[0];
+
+function showNav() {
+  document.getElementById("mobileNav").classList.toggle("onlyOnMobile");
+
+  document.getElementById("mobileNav").classList.toggle("list2");
+}
